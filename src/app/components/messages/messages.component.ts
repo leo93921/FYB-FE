d3a1ffcca7b5ca9c1dc71ea87406a4297dc64206
leo93921 <leonardo.capoccia@hotmail.it>
2017-10-17 18:31:17 +0200

    Add markup to messages

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Communication } from '../../model/communication';
 
 @Component({
   selector: 'app-messages',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
+  public messages: Communication[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    for (const i of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]) {
+      const message = new Communication();
+      message.sendDate = new Date();
+      message.text = `aifubiaubibadf di bsfliabsdasb dkf kuadvfukaudvf kuvaudvkua vd`;
+      if (Math.floor(i % 2) === 0) {
+        message.sentFrom = '2';
+      } else {
+        message.sentFrom = '4';
+      }
+      this.messages.push(message);
+    }
   }
-
 }
